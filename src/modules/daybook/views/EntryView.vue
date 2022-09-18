37dc3fcffbbcb589341566f316dd990293dd2428
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2022-09-18 22:25:06 +0200

    Mostrar entrada en pantalla o redireccionar al usuario

--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -2,9 +2,9 @@
     <div class="entry-title d-flex justify-content-between p-2">
 
         <div>
-            <span class="text-success fs-3 fw-bold">15</span>
-            <span class="mx-1 fs-3">Julio</span>
-            <span class="mx-2 fs-4 fw-light">2012, jueves</span>
+            <span class="text-success fs-3 fw-bold">{{ day }}</span>
+            <span class="mx-1 fs-3">{{ month }}</span>
+            <span class="mx-2 fs-4 fw-light">{{ yearDay }}</span>
         </div>
 
         <div>
@@ -23,6 +23,7 @@
     <hr>
     <div class="d-flex flex-column px-3 h-75">
         <textarea
+            v-model="entry.text"
             placeholder="¿Qué sucedió hoy?"
         ></textarea>
     </div>
@@ -43,6 +44,8 @@
 import { defineAsyncComponent } from 'vue'
 import { mapGetters } from 'vuex'
 
+import getDayMonthYear from '../helpers/getDayMonthYear'
+
 export default {
     props: {
         id: {
@@ -53,17 +56,42 @@ export default {
     components: {
         Fab: defineAsyncComponent(() => import('../components/Fab.vue'))
     },
+    data() {
+        return {
+            entry: null
+        }
+    },
     computed: {
         ...mapGetters('journal', ['getEntryById']),
+        day() {
+            const { day } = getDayMonthYear( this.entry.date )
+            return day
+        },
+        month() {
+            const { month } = getDayMonthYear( this.entry.date )
+            return month
+        },
+        yearDay() {
+            const { yearDay } = getDayMonthYear( this.entry.date )
+            return yearDay
+        }
     },
     methods: {
         loadEntry() {
             const entry = this.getEntryById( this.id )
-            console.log(entry)
+
+            if ( !entry ) return this.$router.push({ name: 'no-entry' })
+
+            this.entry = entry
         }
     },
     created() {
         this.loadEntry()
+    },
+    watch: {
+        id() {
+            this.loadEntry()
+        }
     }
 }
 </script>
