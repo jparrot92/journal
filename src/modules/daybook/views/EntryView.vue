fd6ccdc5e7c02df60efce7e12b61d52750125d98
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2022-09-18 22:35:27 +0200

    Cargar entrada cuando cambia el url

--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -1,38 +1,42 @@
 <template>
-    <div class="entry-title d-flex justify-content-between p-2">
 
-        <div>
-            <span class="text-success fs-3 fw-bold">{{ day }}</span>
-            <span class="mx-1 fs-3">{{ month }}</span>
-            <span class="mx-2 fs-4 fw-light">{{ yearDay }}</span>
+    <template v-if="entry">
+        <div class="entry-title d-flex justify-content-between p-2">
+
+            <div>
+                <span class="text-success fs-3 fw-bold">{{ day }}</span>
+                <span class="mx-1 fs-3">{{ month }}</span>
+                <span class="mx-2 fs-4 fw-light">{{ yearDay }}</span>
+            </div>
+
+            <div>
+                <button class="btn btn-danger mx-2">
+                    Borrar
+                    <i class="fa fa-trash-alt"></i>
+                </button>
+
+                <button class="btn btn-primary">
+                    Subir foto
+                    <i class="fa fa-upload"></i>
+                </button>
+            </div>
         </div>
 
-        <div>
-            <button class="btn btn-danger mx-2">
-                Borrar
-                <i class="fa fa-trash-alt"></i>
-            </button>
-
-            <button class="btn btn-primary">
-                Subir foto
-                <i class="fa fa-upload"></i>
-            </button>
+        <hr>
+        <div class="d-flex flex-column px-3 h-75">
+            <textarea
+                v-model="entry.text"
+                placeholder="¿Qué sucedió hoy?"
+            ></textarea>
         </div>
-    </div>
-
-    <hr>
-    <div class="d-flex flex-column px-3 h-75">
-        <textarea
-            v-model="entry.text"
-            placeholder="¿Qué sucedió hoy?"
-        ></textarea>
-    </div>
 
 
-    <img 
-        src="https://www.robertlandscapes.com/wp-content/uploads/2014/11/landscape-322100_1280.jpg" 
-        alt="entry-picture"
-        class="img-thumbnail">
+        <img 
+            src="https://www.robertlandscapes.com/wp-content/uploads/2014/11/landscape-322100_1280.jpg" 
+            alt="entry-picture"
+            class="img-thumbnail">
+
+    </template>
 
     <Fab 
         icon="fa-save"
