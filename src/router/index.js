e6484a9ac4fab1e07f061c6fd0b35dd3fbad1810
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2022-10-02 23:29:23 +0200

    Cambios nombre vistas

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/Home.vue'
 
 import daybookRouter from '../modules/daybook/router'
 
@@ -15,7 +15,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: '/daybook',
