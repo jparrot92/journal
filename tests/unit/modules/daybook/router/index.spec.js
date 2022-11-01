8fd1b198b1700a67806efa7a55cea57ff548a5bf
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2022-11-02 00:41:56 +0100

    Probar función de mapeo de ruta

--- tests/unit/modules/daybook/router/index.spec.js
+++ tests/unit/modules/daybook/router/index.spec.js
@@ -23,10 +23,29 @@ describe('Pruebas en el router module del Daybook', () => {
             ]
         })
 
-        expect( (await daybookRouter.children[0].component()).default.name  ).toBe('NoEntrySelected')
-        expect( (await daybookRouter.children[1].component()).default.name  ).toBe('EntryView')
+        // expect( (await daybookRouter.children[0].component()).default.name  ).toBe('NoEntrySelected')
+        // expect( (await daybookRouter.children[1].component()).default.name  ).toBe('EntryView')
+        const promiseRoutes = []
+        daybookRouter.children.forEach( child => promiseRoutes.push( child.component() ) )
 
+        const routes = (await Promise.all( promiseRoutes )).map( r => r.default.name )
 
+        expect( routes ).toContain('EntryView')
+        expect( routes ).toContain('NoEntrySelected')
+
+    })
+
+    test('debe de retornar el id de la ruta', () => {
+        
+        const route = {
+            params: {
+                id: 'ABC-123'
+            }
+        }
+
+        // expect( daybookRouter.children[1].props( route ) ).toEqual({ id: 'ABC-123' })
+        const entryRoute = daybookRouter.children.find( route => route.name === 'entry' )
+        expect( entryRoute.props( route ) ).toEqual({ id: 'ABC-123' })
     })
 
 })
