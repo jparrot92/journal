dbc4f5b595dd565098e308c7990b4a04deb9e4e3
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2022-11-16 19:29:05 +0100

    Borrar archivos subidos en las pruebas

--- tests/unit/modules/daybook/helpers/uploadImage.spec.js
+++ tests/unit/modules/daybook/helpers/uploadImage.spec.js
@@ -1,5 +1,14 @@
+import 'setimmediate'
+import cloudinary from 'cloudinary'
 import axios from 'axios'
+
 import uploadImage from '@/modules/daybook/helpers/uploadImage'
+
+cloudinary.config({
+    cloud_name: 'dv5fhqjdo',
+    api_key: '118888497155164',
+    api_secret: '9L3ewBORJlH3gEjUuy3EIcF9zyw'
+})
 
 describe('Pruebas en el uploadImage', () => {
     
@@ -15,7 +24,15 @@ describe('Pruebas en el uploadImage', () => {
 
         expect( typeof url ).toBe('string')
 
-        
+        // Tomar el ID
+        const segments = url.split('/')
+        const imageId = segments[ segments.length - 1 ].replace('.jpg','')
+
+        // Eliminar imagen
+        const { deleted } = await cloudinary.v2.api.delete_resources(imageId)
+
+        // Comprueba que la imagen haya sido eliminada
+        expect(deleted[imageId]).toBe('deleted')
     })
 
     
