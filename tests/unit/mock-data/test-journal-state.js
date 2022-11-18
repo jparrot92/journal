5cefa4ba2342f3217b73fdf73c84a69d48d60f14
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2022-11-18 21:50:53 +0100

    Vuex - Probar el estado inicial del Journal

---
+++ tests/unit/mock-data/test-journal-state.js
@@ -0,0 +1,17 @@
+export const journalState = {
+    isLoading: false,
+    entries: [
+        {
+            id: '-NDF1PyxyfLxOtMc_1gW',
+            date: 1664568863009,
+            picture: 'https://res.cloudinary.com/dv5fhqjdo/image/upload/v1664579042/vpubxhkfovxbd40dpghm.webp',
+            text: 'dsdasd s dsadas dasgh 454 fgdf'
+        },
+        {
+            id: '-NDFC6EV7Y-gU0H_bSk_',
+            date: 1664571667395,
+            picture: 'https://res.cloudinary.com/dv5fhqjdo/image/upload/v1664581355/g6u7ptyjsodetswdhdcq.jpg',
+            text: 'erdsfadsf dafs da'
+        }
+    ]
+}
