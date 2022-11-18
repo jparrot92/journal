1ee360a2dd497549c3614319e490df1d85feb414
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2022-11-18 23:54:46 +0100

    Mutation - updateEntry

--- tests/unit/modules/daybook/store/journal/journal-module.spec.js
+++ tests/unit/modules/daybook/store/journal/journal-module.spec.js
@@ -40,5 +40,32 @@ describe('Vuex - Pruebas en el Journal Module', () => {
 
     })
 
+    test('mutation: updateEntry', () => {
+    
+        // create store con entries
+        const store = createVuexStore( journalState )
+        const updatedEntry = {
+            id: '-NDF1PyxyfLxOtMc_1gW',
+            date: 1664568863009,
+            picture: 'https://res.cloudinary.com/dv5fhqjdo/image/upload/v1664579042/vpubxhkfovxbd40dpghm.webp',
+            text: 'Hola, que tal?'
+        }
+
+        // commit de la mutación
+        store.commit('journal/updateEntry', updatedEntry)
+
+        const storeEntries = store.state.journal.entries
+
+        // Expects
+        // entries.lenght =2
+        expect(storeEntries.length).toBe(2)
+
+        // entries tiene que existir updatedEntry toEqual
+        expect( 
+            storeEntries.find( e => e.id === updatedEntry.id )
+        ).toEqual( updatedEntry )
+
+    })
+
 
 })
