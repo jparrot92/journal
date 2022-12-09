04b922560b2fb5eb59709d53e248a3916b75bae5
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2022-12-09 23:36:04 +0100

    Diseño del Layout de autenticación

---
+++ src/modules/auth/router/index.js
@@ -0,0 +1,7 @@
+export default {
+    name: 'auth',
+    component: () => import(/* webpackChunkName: "auth" */ '@/modules/auth/layouts/AuthLayout.vue'),
+    children: [
+        
+    ]
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/Home.vue'
-
+import authRouter from '../modules/auth/router'
 import daybookRouter from '../modules/daybook/router'
 
 const routes = [
@@ -16,6 +16,10 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/auth',
+    ...authRouter
   },
   {
     path: '/daybook',
