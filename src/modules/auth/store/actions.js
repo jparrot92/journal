9c5dd35f56199aecef01f2ac7b89d1897f4845bd
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2022-12-10 14:50:21 +0100

    Mutation - LoginUser

--- src/modules/auth/store/actions.js
+++ src/modules/auth/store/actions.js
@@ -4,15 +4,15 @@ export const createUser = async ({ commit }, user) => {
 
     const { name, email, password } = user
 
-    console.log(commit)
-
     try {
 
         const { data } = await authApi.post(':signUp', { email, password, returnSecureToken: true })
-        const { idToken } = data
+        const { idToken, refreshToken } = data
 
-        const resp = await authApi.post(':update', { displayName: name, idToken })
-        console.log(resp)
+        await authApi.post(':update', { displayName: name, idToken })
+        
+        delete user.password
+        commit('loginUser', { user, idToken, refreshToken })
 
         return { ok: true}
 
