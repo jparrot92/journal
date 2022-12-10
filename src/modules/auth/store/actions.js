ee6904041229b353544caa9dbbf040e2ff5a4706
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2022-12-10 23:47:51 +0100

    Actions - Login de usuario

--- src/modules/auth/store/actions.js
+++ src/modules/auth/store/actions.js
@@ -17,7 +17,26 @@ export const createUser = async ({ commit }, user) => {
         return { ok: true}
 
     } catch (error) {
-        console.log(error.response)
+        return { ok: false, message: error.response.data.error.message }
+    }
+
+}
+
+export const signInUser = async ({ commit }, user) => {
+
+    const { email, password } = user
+
+    try {
+
+        const { data } = await authApi.post(':signInWithPassword', { email, password, returnSecureToken: true })
+        const { displayName, idToken, refreshToken } = data
+        user.name = displayName
+
+        commit('loginUser', { user, idToken, refreshToken })
+
+        return { ok: true}
+
+    } catch (error) {
         return { ok: false, message: error.response.data.error.message }
     }
 
