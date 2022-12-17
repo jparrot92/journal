ce0c753e6884fa2c4d01e90202789713ee2f7555
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2022-12-17 12:44:40 +0100

    Interceptores

--- src/api/journalApi.js
+++ src/api/journalApi.js
@@ -4,6 +4,15 @@ const journalApi = axios.create({
     baseURL: 'https://journal-api-fbd6b-default-rtdb.europe-west1.firebasedatabase.app'
 })
 
+journalApi.interceptors.request.use( (config) => {
+
+    config.params = {
+        auth: localStorage.getItem('idToken')
+    }
+
+    return config
+})
+
 // console.log( process.env.NODE_ENV ) // TEST durante testing, 
 
 export default journalApi
