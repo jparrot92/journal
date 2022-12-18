b0d7ddbc496be92f34720bcbdd80d03a80d067a2
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2022-12-18 11:52:00 +0100

    Mutations - logout

--- tests/unit/modules/auth/store/auth-module.spec.js
+++ tests/unit/modules/auth/store/auth-module.spec.js
@@ -19,32 +19,58 @@ describe('Vuex: Pruebas en el auth-module', () => {
         expect( refreshToken ).toBe( null )
     })
 
-        // Mutations
-        test('Mutation: loginUser', () => {
-        
-            const store = createVuexStore({
-                status: 'authenticating', // 'authenticated','not-authenticated', 'authenticating'
-                user: null,
-                idToken: null,
-                refreshToken: null
-            })
+    // Mutations
+    test('Mutation: loginUser', () => {
     
-            const payload = {
-                user: { name: 'test', email: 'test@test.com' },
-                idToken: 'ABC-123',
-                refreshToken: 'XYZ-123'
-            }
-    
-            store.commit('auth/loginUser', payload )
-    
-            const { status, user, idToken, refreshToken } = store.state.auth
-            
-            expect( status ).toBe( 'authenticated' )
-            expect( user ).toEqual( { name: 'test', email: 'test@test.com' } )
-            expect( idToken ).toBe( 'ABC-123' )
-            expect( refreshToken ).toBe( 'XYZ-123' )
-    
+        const store = createVuexStore({
+            status: 'authenticating', // 'authenticated','not-authenticated', 'authenticating'
+            user: null,
+            idToken: null,
+            refreshToken: null
         })
+
+        const payload = {
+            user: { name: 'test', email: 'test@test.com' },
+            idToken: 'ABC-123',
+            refreshToken: 'XYZ-123'
+        }
+
+        store.commit('auth/loginUser', payload )
+
+        const { status, user, idToken, refreshToken } = store.state.auth
+        
+        expect( status ).toBe( 'authenticated' )
+        expect( user ).toEqual( { name: 'test', email: 'test@test.com' } )
+        expect( idToken ).toBe( 'ABC-123' )
+        expect( refreshToken ).toBe( 'XYZ-123' )
+
+    })
+
+    test('Mutation: logout', () => {
+
+        localStorage.setItem('idToken','ABC-123')
+        localStorage.setItem('refreshToken','XYZ-123')
+      
+        const store = createVuexStore({
+            status: 'authenticated', // 'authenticated','not-authenticated', 'authenticating'
+            user: { name: 'fernando', email: 'fer@gmail.com' },
+            idToken: 'ABC-123',
+            refreshToken: 'XYZ-456'
+        })
+
+        store.commit('auth/logout')
+
+        const { status, user, idToken, refreshToken } = store.state.auth
+        
+        expect( status ).toBe( 'not-authenticated' )
+        expect( user ).toBeFalsy()
+        expect( idToken ).toBeFalsy()
+        expect( refreshToken ).toBeFalsy()
+
+        expect( localStorage.getItem('idToken') ).toBeFalsy()
+        expect( localStorage.getItem('refreshToken') ).toBeFalsy()
+
+    })
     
 
 })
