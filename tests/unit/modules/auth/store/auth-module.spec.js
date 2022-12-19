e31dc1728cb1c6549c61c62239652527f6481c7a
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2022-12-19 17:27:14 +0100

    Actions - createUser - Error usuario ya existe

--- tests/unit/modules/auth/store/auth-module.spec.js
+++ tests/unit/modules/auth/store/auth-module.spec.js
@@ -86,6 +86,29 @@ describe('Vuex: Pruebas en el auth-module', () => {
         expect( store.getters['auth/username'] ).toBe('test')
 
     })
+
+    // Actions
+    test('Actions: createUser - Error usuario ya existe', async() => {
+    
+        const store = createVuexStore({
+            status: 'not-authenticated', // 'authenticated','not-authenticated', 'authenticating'
+            user: null,
+            idToken: null,
+            refreshToken: null
+        })
+
+        const newUser = { name: 'Test User', email: 'test@test.com', password: '123456' }
+
+        const resp = await store.dispatch('auth/createUser', newUser )
+        expect( resp ).toEqual({ ok: false, message: 'EMAIL_EXISTS' })
+
+        const { status, user, idToken, refreshToken } = store.state.auth
+        
+        expect( status ).toBe( 'not-authenticated' )
+        expect( user ).toBeFalsy()
+        expect( idToken ).toBeFalsy()
+        expect( refreshToken ).toBeFalsy()
+    })
     
 
 })
