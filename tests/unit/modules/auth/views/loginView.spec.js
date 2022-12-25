69f0a76b76f2458e3867723bb720205390d9169c
Jaume Parrot Altisent <jaumeparrot2@gmail.com>
2022-12-25 20:20:15 +0100

    Formulario incorrecto, retorna error de autenticación

--- tests/unit/modules/auth/views/loginView.spec.js
+++ tests/unit/modules/auth/views/loginView.spec.js
@@ -4,6 +4,14 @@ import LoginView from '@/modules/auth/views/LoginView.vue'
 import createVuexStore from '../../../mock-data/mock-store'
 import createVueRouter from '../../../mock-data/mock-router'
 
+import Swal from 'sweetalert2'
+
+jest.mock('sweetalert2', () => ({
+    fire: jest.fn(),
+    showLoading: jest.fn(),
+    close: jest.fn()
+}))
+
 describe('Pruebas en el LoginView Component', () => {
 
     const router = createVueRouter()
@@ -14,6 +22,8 @@ describe('Pruebas en el LoginView Component', () => {
         idToken: null,
         refreshToken: null
     })
+
+    store.dispatch = jest.fn()
 
     beforeEach(() => jest.clearAllMocks() )
 
@@ -26,6 +36,21 @@ describe('Pruebas en el LoginView Component', () => {
         })
 
         expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    test('credenciales incorrectas, disparar el SWAL', async() => {
+        
+        store.dispatch.mockReturnValueOnce({ ok: false, message: 'Error en credenciales' })
+
+        const wrapper = shallowMount( LoginView, {
+            global: {
+                plugins: [ store ]
+            }
+        })
+
+        await wrapper.find('form').trigger('submit')
+        expect( store.dispatch ).toHaveBeenCalledWith('auth/signInUser', { email: '', password: ''})
+        expect( Swal.fire ).toHaveBeenCalledWith('Error', 'Error en credenciales', 'error')
     })
 
 })
